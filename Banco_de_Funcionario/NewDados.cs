46c05ae4c428e4bac5655875e7fbcc5ae5969bef
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-07-09 01:24:58 -0300

    projeto de banco de funcionarios e cadastro

---
+++ Banco_de_Funcionario/NewDados.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace Banco_de_Funcionario {
+    internal class NewData {
+
+        public int UserId;
+        public string Nome;
+        public string Sobrenome;
+        public string Cargo;
+        public double Salario;
+        public char Sexo;
+
+
+        public NewData() {
+
+        }
+
+        public NewData(int userId, string nome, string sobrenome) {
+            UserId = userId;
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public NewData(int userId, string nome, string sobrenome, string cargo, double salario, char sexo) : this(userId, nome, sobrenome) {
+            Cargo = cargo;
+            Salario = salario;
+            Sexo = sexo;
+        }
+
+        public string Text() {
+            Console.WriteLine("       ==============================================================================================");
+            Console.WriteLine("       ==============================================================================================");
+            Console.WriteLine("                                        >>>> FUNCIONÁRIO CADASTRADO <<<<");
+            Console.WriteLine("       ==============================================================================================");
+            Console.WriteLine("       ==============================================================================================");
+
+            return " ";
+        }
+
+        
+
+        override public string ToString() {
+            Console.WriteLine(Text());
+            Console.WriteLine();
+            return $" Id do Usuário: {UserId} || Nome do funcionário: {Nome} {Sobrenome} || Sexo: {Sexo} || Cargo: {Cargo} || Salário: {Salario} reais ";
+        }
+    }
+}
---
+++ Banco_de_Funcionario/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+
+namespace Banco_de_Funcionario {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("========================");
+            Console.WriteLine("   § CASDASBANK §");
+            Console.WriteLine();
+            Console.WriteLine("    CADASTRE AQUI");
+            Console.WriteLine(" SEU NOVO FUNCIONÁRIO");
+            Console.WriteLine("========================");
+            Console.WriteLine();
+
+            Console.WriteLine("^^^^^^^^^^^^");
+            Console.WriteLine("FAÇA O LOGIN");
+            Console.WriteLine("vvvvvvvvvvvv");
+            Console.WriteLine();
+
+            Console.Write("DIGITE O USUÁRIO: ");
+            string usuario = Console.ReadLine();
+            Console.Write("DIGITE A SENHA: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            string resposta;
+
+            if(usuario == "Alan" &&  senha == 1234 ) {
+
+                Console.WriteLine();
+                Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                Console.WriteLine("         BEM VINDO MEU QUERIDO");
+                Console.WriteLine("  VAMOS LÁ, DIGITE AS INFORMAÇÕES ABAIXO");
+                Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+                Console.WriteLine();
+                Console.Write("Digite o Id que será usado pelo novo funcionário (numérico): ");
+                int userId = int.Parse(Console.ReadLine());
+                Console.Write("Digite o nome do funcionário: ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite o sobrenome do funcionário: ");
+                string sobrenome = Console.ReadLine();
+                Console.Write("Digite o cargo que o funcionário irá exercer: ");
+                string cargo = Console.ReadLine();
+                Console.Write("Digite o salário que o funcionário irá receber: ");
+                double salario = double.Parse(Console.ReadLine());
+                Console.Write("Sexo do funcionário (Digite 'F' para feminino e 'M' para masculino): ");
+                char sexo = char.Parse(Console.ReadLine());
+
+                NewData dados = new NewData(userId, nome, sobrenome, cargo, salario, sexo);
+
+                Console.WriteLine();
+                Console.WriteLine(dados);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("       §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§");
+                Console.WriteLine("       §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§");
+                Console.WriteLine("                                              PARA UM NOVO CADASTRO");
+                Console.WriteLine("                                           OU PARA ENCERRAR O CADASBANK");
+                Console.WriteLine("                                              CLIQUE NA TECLA ENTER");
+                Console.WriteLine("       §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§");
+                Console.WriteLine("       §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§");
+                Console.WriteLine();
+
+            }
+            else {
+                Console.WriteLine();
+                Console.WriteLine("  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+                Console.WriteLine("   SENHA OU USUÁRIO INCORRETO,");
+                Console.WriteLine("     CLIQUE NA TECLA ENTER");
+                Console.WriteLine("       E TENTE NOVAMENTE!");
+                Console.WriteLine("   >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            }
+
+        }
+    }
+}
